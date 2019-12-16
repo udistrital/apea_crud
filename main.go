d227b3a83ba73d54e71d38e8b160d5d481e9e602
simarhj <seherreraj@udistrital.edu.co>
2019-12-16 17:47:17 -0500

    variables de entorno

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	_ "github.com/simarhj/apea_crud/routers"
-	"github.com/astaxie/beego/plugins/cors"
+	//apistatus "github.com/udistrital/utils_oas/apiStatusLib"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/plugins/cors"
 	_ "github.com/lib/pq"
 )
 
+func init() {
+	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGurls")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+beego.AppConfig.String("PGschemas")+"")
+}
+
 func main() {
 	orm.Debug = true
-	orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
+	//orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
@@ -19,14 +25,18 @@ func main() {
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
 		AllowHeaders: []string{"Origin", "x-requested-with",
-		  "content-type",
-		  "accept",
-		  "origin",
-		  "authorization",
-		  "x-csrftoken"},
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
-	  }))
+	}))
+
+	logPath := "{\"filename\":\""
+	logPath += beego.AppConfig.String("logPath")
+	logPath += "\"}"
+	logs.SetLogger(logs.AdapterFile, logPath)
 	beego.Run()
 }
-
